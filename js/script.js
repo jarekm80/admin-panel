9aa76ca2ff8a76e912e04d3d25c9a6fbc4cdb8f2
Jarek Matwijewicz <alterro@gmail.com>
2021-04-13 14:25:03 +0200

    add top Panel

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+const app = {
+  initDates: function(){
+    const thisDates=this;
+
+    thisDates.dom={};
+    thisDates.dom.dateStart = document.querySelector(`input[name="dateStart"]`);
+    thisDates.dom.dateEnd =   document.querySelector(`input[name="dateEnd"]`);
+    thisDates.defDate = new Date();
+    // eslint-disable-next-line no-undef
+    flatpickr(thisDates.dom.dateStart,{defaultDate: thisDates.defDate,});
+    // eslint-disable-next-line no-undef
+    flatpickr(thisDates.dom.dateEnd,{defaultDate: thisDates.defDate,});
+  },
+
+  init: function(){
+    const thisApp = this;
+    thisApp.initDates();
+  },
+};
+
+app.init();
