7cf370f37e696ba82a4dbdd97fd3008098765945
Jarek Matwijewicz <alterro@gmail.com>
2021-04-25 21:14:46 +0200

    add modal

--- js/script.js
+++ js/script.js
@@ -32,3 +32,54 @@ document.querySelector('.hamburger').addEventListener('click', function(e) {
 
   toggleMenu();
 });
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  var isFormValidate = true;
+
+  var emailAddressInput = event.target.querySelector('input[name="email"]');
+  if(emailAddressInput.value.indexOf('@') < 0) {
+    isFormValidate = false;
+    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+});
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if(e.key === 'Escape') {
+    closeModal();
+  }
+});
+
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+//let modalYourManager = document.querySelector('#modalYourManager');
+
+document.querySelector('#yourManager').addEventListener('click', function(e) {
+  e.preventDefault();
+  openModal('#modalYourManager');
+});
